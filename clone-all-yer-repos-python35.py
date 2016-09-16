2c43a2fc1c64b51d89b6875d120b17b0025fa8b8
Andrew <andrew.kuttor@gmail.com>
2016-09-16 09:33:26 -0700

    Swapped urllib2 for requests, fixed style. Added functions for get url and get pages.

---
+++ clone-all-yer-repos-python35.py
@@ -0,0 +1,50 @@
+#!/usr/bin/usr python
+"""
+clone-all-yer-repos - Clones all repos by username
+
+Usage:
+  clone-all-yer-repos <username>
+  clone-all-yer-repos [ -h | --help | --version ]
+
+Options:
+  --version               Show version
+  -h --help               Show this screen
+"""
+
+import requests
+from docopt import DocoptExit, docopt
+
+
+# about
+__author__ = "Andrew Kuttor "
+__maintainer__ = "Andrew Kuttor"
+__email__ = "akuttor@gmail.com"
+__version__ = "1.0.0"
+
+
+def main():
+    try:
+        args = docopt(__doc__, version='Clone All Yer Repos - v1.0')
+        print(args)
+        get_total_pages(args['<username>'])
+
+        # get_total_pages(**args)
+    except DocoptExit as e:
+        print(e.message)
+
+def get_github_url_as_json(user):
+    url = 'https://api.github.com/users/{0}'.format(user)
+    r = requests.get(url)
+    print(r.text)
+    return(r.text)
+
+
+# get total repos and divivde by 100
+# use requests to snag attribute - "public_repos": X
+def get_total_number_pages_of_url():
+    return "place holder"
+
+
+
+if __name__ == "__main__":
+    exit(main())
