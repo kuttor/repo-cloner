1a6653a59df23489b51d00f350457b8298d04193
Andrew <andrew.kuttor@gmail.com>
2016-09-18 15:57:05 -0700

    lots of reworking.

--- clone-all-yer-repos-python35.py
+++ clone-all-yer-repos-python35.py
@@ -12,38 +12,47 @@ Options:
   -h --help               Show this screen
 """
 
+import git
 from requests import get
 from pprint import pprint
 from docopt import DocoptExit, docopt
 
 
 # about
-__author__ = "Andrew Kuttor "
-__maintainer__ = "Andrew Kuttor"
-__email__ = "akuttor@gmail.com"
-__version__ = "1.0.0"
+__author__ = 'Andrew Kuttor'
+__maintainer__ = 'Andrew Kuttor'
+__email__ = 'akuttor@gmail.com'
+__version__ = '1.0.0'
 
 
 def main():
     try:
         args = docopt(__doc__, version='Clone All Yer Repos - v1.0')
-        print(args)
-        url_to_json(args['<username>'])
+        total_repos = github_to_json(account=args['<username>'])['public_repos']
+        clone_repos(total_repos=total_repos, account=args['<username>'])
+
     except DocoptExit as e:
         print(e.message)
 
 
-def url_to_json(user):
-    url = 'https://api.github.com/users/{0}'.format(user)
-    pprint(get(url).json()) # test output
-    return(get(url).json())
+def github_to_json(account):
+    url = 'https://api.github.com/users/{}/repos'.format(account)
+    json = (get(url).json())
+    return(json)
 
 
-# get total repos and divivde by 100
-# use requests to snag attribute - "public_repos": X
-def get_total_number_pages_of_url():
-    return "place holder"
+def clone_repos(total_repos, account):
+    url = 'https://api.github.com/users/{}/repos?'.format(account)
+    num = 0
+    for num in url:
+        if num <= total_repos:
+            url = url + 'page={}per_page=1'.format(num)
+            print(url['0']['git_url'])
 
 
-if __name__ == "__main__":
+def total_repos(total_repos):
+    return 'place holder'
+
+
+if __name__ == '__main__':
     exit(main())
