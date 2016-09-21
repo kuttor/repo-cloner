7fd01e0833647a0776656f63e0508789ab417dd2
Andrew <andrew.kuttor@gmail.com>
2016-09-20 18:09:30 -0700

    file change

--- clone-all-yer-repos.py
+++ clone-all-yer-repos.py
@@ -1,41 +1,57 @@
 #!/usr/bin/usr python
 """
-clone-all-yer-repos - Clones all repos by username
+clone-all-yer-repos - Clones all public github repos in an account
 
 Usage:
-  clone-all-yer-repos -u <username>
-  clone-all-yer-repos [ -h | --help | --version ]
+  clone-all-yer-repos <username>
+  clone-all-yer-repos [ -h | --help ]
+  clone-all-yer-repos [ -v | --version ]
 
 Options:
-  --version               Show version
+  -v --version            Show version
   -h --help               Show this screen
-  -u --username USERNAME  Specify github username to create
 """
-from __future__ import absolute_import, division, print_function
-from urllib2 import urlopen
+
+import git
+from requests import get
+from pprint import pprint
 from docopt import DocoptExit, docopt
 
 
 # about
-__author__ = "Andrew Kuttor "
-__maintainer__ = "Andrew Kuttor"
-__email__ = "akuttor@gmail.com"
-__version__ = "1.0.0"
+__author__ = 'Andrew Kuttor'
+__maintainer__ = 'Andrew Kuttor'
+__email__ = 'akuttor@gmail.com'
+__version__ = '1.0.0'
 
 
 def main():
     try:
         args = docopt(__doc__, version='Clone All Yer Repos - v1.0')
-        print args
-        # get_total_pages(**args)
+        total_repos = github_to_json(account=args['<username>'])['public_repos']
+        clone_repos(total_repos=total_repos, account=args['<username>'])
     except DocoptExit as e:
-        print e.message
+        print(e.message)
 
-# get total repos and divivde by 100
-# use urllib2 to snag attribute - "public_repos": X
-def get_total_pages(user):
-    url = urlopen('https://api.github.com/users/{0}').format(user)
-    print url.read()
 
-if __name__ == "__main__":
+def github_to_json(account):
+    url = 'https://api.github.com/users/{}/repos'.format(account)
+    json = (get(url).json())
+    return(json)
+
+
+def clone_repos(total_repos, account):
+    url = 'https://api.github.com/users/{}/repos?'.format(account)
+    num = 0
+    for num in url:
+        if num <= total_repos:
+            url = url + 'page={}per_page=1'.format(num)
+            print(url['0']['git_url'])
+
+
+def total_repos(total_repos):
+    return 'place holder'
+
+
+if __name__ == '__main__':
     exit(main())
